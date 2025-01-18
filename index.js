13a6ce8ae8a59315d6e3582a49a8fb6b73117f4c
MoWagih <mowagih2@gmail.com>
2025-01-18 14:55:02 +0200

    first-commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let burgerMenuStatus = false;
+
+// function toggleBurgerMenuNav(){
+//     document.getElementsByClassName("burger-menu").style.display = "none";
+//     if(burgerMenuStatus == false) {
+//         burgerMenuStatus == true;
+//         document.getElementsByClassName("burger-menu").style.position = "relative";
+//         console.log("nav is open")
+//     }
+// }
